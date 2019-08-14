5062646d83b7ee9cfabc2e0c67d25710201b109c
James Fenwick <james.fenwick@iamproperty.com>
2019-08-14 11:04:28 +0100

    Add service provider

---
+++ src/AssetServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace IAMProperty\SharedAssets;
+
+use Illuminate\Support\ServiceProvider;
+
+class AssetServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+}
