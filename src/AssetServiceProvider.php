8f1166d65659f286bd7be6dd20fa8097330c014d
James Fenwick <james.fenwick@iamproperty.com>
2019-08-14 13:10:20 +0100

    Add brand logos

--- src/AssetServiceProvider.php
+++ src/AssetServiceProvider.php
@@ -8,6 +8,8 @@ class AssetServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //
+        $this->publishes([
+            __DIR__.'/../resources/assets/brand-logos' => public_path('img/brand-logos')
+        ], ['assets', 'logos']);
     }
 }
