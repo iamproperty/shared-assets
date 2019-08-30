5ca838814a5c79531ade36003b64f586c045414e
James Fenwick <james.fenwick@iamproperty.com>
2019-08-30 16:07:33 +0100

    Update header classes

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,10 +1,10 @@
 <header class="iam-header">
-    <div class="iam-header__services">
+    <div class="iam-header__section iam-header__section--services">
         {{ $slot }}
     </div>
 
-    <div class="iam-header__menu-toggle">
-        <button type="button" id="iam-header-menu-toggle">
+    <div class="iam-header__section iam-header__section--toggle">
+        <button type="button" id="iam-header-menu-toggle" class="iam-header__toggle">
             <img src="{{ asset('/img/icons/menu.svg') }}" alt="open menu">
         </button>
     </div>
