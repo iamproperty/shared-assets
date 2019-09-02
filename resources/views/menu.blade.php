195848100f94649d30a9222852bb73faf3e5a60a
James Fenwick <james.fenwick@iamproperty.com>
2019-09-02 15:59:26 +0100

    Change close icon url to match @iamproperty/branding

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,7 +1,7 @@
 <div class="iam-menu" id="iam-menu">
     <div class="iam-menu__section iam-menu__section--close">
         <button type="button" class="iam-menu__close" id="iam-menu-close-button" data-close="menu" data-target="#iam-menu">
-            <img src="{{ asset('/img/icons/orange-close-cross.svg') }}" alt="close menu">
+            <img src="{{ asset('/img/icons/close-cross.svg') }}" alt="close menu">
         </button>
     </div>
 
