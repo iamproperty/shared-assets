483122085c9b7d89e4b58e97f14f2e16b620716a
James Fenwick <james.fenwick@iamproperty.com>
2019-09-02 15:22:06 +0100

    Add attributes to the header to open the menu

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,7 +4,7 @@
     </div>
 
     <div class="iam-header__section iam-header__section--toggle">
-        <button type="button" id="iam-header-menu-toggle" class="iam-header__toggle">
+        <button type="button" id="iam-header-menu-toggle" class="iam-header__toggle" data-toggle="menu" data-target="#iam-menu">
             <img src="{{ asset('/img/icons/menu.svg') }}" alt="open menu">
         </button>
     </div>
